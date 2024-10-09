8973ba1d9c8223364002b0eeacab8a5bd7d88ddf
Your Name <you@example.com>
2024-10-09 21:04:37 +0300

    templates

---
+++ templates/jobs.php
@@ -0,0 +1,18 @@
+<?php require_once  dirname(__FILE__) . "/menu.php" ?>
+
+<div id="app-content">
+	<table id="myTable" class="display">
+		<thead>
+		<tr>
+			<th>Name</th>
+			<th>Email subject</th>
+		</tr>
+		</thead>
+		<tbody>
+
+		</tbody>
+	</table>
+
+</div>
+
+
