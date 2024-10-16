2018f8d046c9c2200c8e12356ec0b3103dcc17f7
mgundelfinger <marlongundelfinger@proton.me>
2024-10-16 15:40:01 +0200

    Fix Licence and Copyright

--- templates/myMainTemplate.php
+++ templates/myMainTemplate.php
@@ -1,4 +1,7 @@
-<?php require_once  dirname(__FILE__) . "/menu.php" ?>
+<?php 
+// SPDX-FileCopyrightText: Marlon Gundelfinger <marlonqgundelfinger@gmail.com>
+// SPDX-License-Identifier: AGPL-3.0-or-later
+require_once  dirname(__FILE__) . "/menu.php" ?>
 
 
 
