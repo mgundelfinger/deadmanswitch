fdeb9c95f204dbd361351f1e7ffd006a259dd3fb
Your Name <you@example.com>
2024-10-20 15:09:08 +0300

    fix

--- templates/myMainTemplate.php
+++ templates/myMainTemplate.php
@@ -5,24 +5,23 @@
 <div id="app-content">
 
 
-    <form>
-
-    <div>
-        <input id="onOffSwitch" type="checkbox">Dead Man Switch</input>
-    </div>
-    <div id="config">
-        <select id="intervalSelector" disabled>
-            <!-- TODO prototype only /-->
-            <option value=0>Instant</option>
-
-            <option value=1>Daily</option>
-            <option value=7>Weekly</option>
-            <option value=28>Every Four Weeks</option>
-        </select>
-    </div>
-    <div>
-        <button id="saveButton" disabled>Save</button>
-        <button id="cancelButton" disabled>Cancel</button>
-    </div>
-    </form>
+<!--    <form>-->
+<!---->
+<!--    <div>-->
+<!--        <input id="onOffSwitch" type="checkbox">Dead Man Switch</input>-->
+<!--    </div>-->
+<!--    <div id="config">-->
+<!--        <select id="intervalSelector" disabled>-->
+<!--            <option value=0>Instant</option>-->
+<!---->
+<!--            <option value=1>Daily</option>-->
+<!--            <option value=7>Weekly</option>-->
+<!--            <option value=28>Every Four Weeks</option>-->
+<!--        </select>-->
+<!--    </div>-->
+<!--    <div>-->
+<!--        <button id="saveButton" disabled>Save</button>-->
+<!--        <button id="cancelButton" disabled>Cancel</button>-->
+<!--    </div>-->
+<!--    </form>-->
 </div>
